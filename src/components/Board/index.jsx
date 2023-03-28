669144dbb3e0a955c2fab8e025bf96e88b66e5ed
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2023-03-28 10:56:42 +0500

    add package react-beautiful-dnd

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 import { Column } from '../Column';
 
 import styles from './Board.module.scss';
 
 export const Board = () => {
+  const onDragEnd = value => {
+    console.log('ondragend', value);
+  };
   return (
     <div className={styles.board}>
       <h2 className={styles.title}>Заявки 22</h2>
-      <div className={styles.taskPanel}>
-        <Column title="Новые" items={4} />
-        <Column title="Текущие" items={7} />
-        <Column title="Закрытые" items={0} />
-        <Column title="Архив" items={3} />
-        <Column title="Удаленные" items={8} />
-      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className={styles.taskPanel}>
+          <Column title="Новые" items={4} />
+          <Column title="Текущие" items={7} />
+          <Column title="Закрытые" items={0} />
+          <Column title="Архив" items={3} />
+          <Column title="Удаленные" items={8} />
+        </div>
+      </DragDropContext>
     </div>
   );
 };
--- src/components/Column/index.jsx
+++ src/components/Column/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
 
 import { Card } from '../Card';
 
@@ -6,7 +7,7 @@ import { ReactComponent as AddFilesSVG } from '../../assets/add-files.svg';
 import styles from './Column.module.scss';
 
 export const Column = props => {
-  const { title, items } = props;
+  const { title, items, droppableId = 'one' } = props;
   return (
     <div className={styles.column}>
       <div className={styles.columnHeader}>
@@ -20,20 +21,27 @@ export const Column = props => {
           <p className={styles.text}>Если есть подходящие заявки, перетащите их сюда 🤓</p>
         </div>
       ) : (
-        <div className={styles.columnBody}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <Droppable droppableId={droppableId}>
+          {(provided, snapshot) => (
+            <div className={styles.columnBody} {...provided.droppableProps} ref={provided.innerRef}>
+              {[1, 2, 3].map((item, index) => (
+                <Draggable key={index} draggableId={String(item)} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className={styles.item}
+                    >
+                      <Card key={item} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
       )}
     </div>
   );
